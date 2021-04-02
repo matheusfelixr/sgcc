f5ba5827342be45216d8139e838e8d6bfe02e7c3
Matheus Felix Ribeiro <matheusfelixr@hotmail.com>
2021-04-02 01:32:48 -0300

    main

---
+++ src/test/java/com/matheusfelixr/sgcc/SgccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusfelixr.sgcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
