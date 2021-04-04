8f3c2159adca3c9f71f3a67431c2a570db377f16
Matheus Felix Ribeiro <matheusfelixr@hotmail.com>
2021-04-04 15:15:42 -0300

    main

--- src/main/java/com/matheusfelixr/sgcc/controller/PointControlController.java
+++ src/main/java/com/matheusfelixr/sgcc/controller/PointControlController.java
@@ -10,10 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.xml.bind.ValidationException;
@@ -34,7 +31,7 @@ public class PointControlController {
     @Autowired
     private PointControlService pointControlService;
 
-    @PostMapping(value = "/registry-point")
+    @GetMapping(value = "/registry-point")
     @ApiOperation(value = "Método responsável por registrar ponto.")
     public ResponseEntity<ResponseApi<MessageDTO>> registryPoint(HttpServletRequest httpServletRequest) throws Exception {
         LOGGER.debug("Inicio processo de registro um novo ponto.");
